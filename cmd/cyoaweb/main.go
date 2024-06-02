305a8b11f923525b4d1677b3ab67d8bbe7c6033d
nemopss <alexey_gladilin@mail.ru>
2024-06-02 18:32:57 +0300

    Added HTTP handler

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/nemopss/cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA application on")
 	filename := flag.String("file", "story.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s\n", *filename)
@@ -23,5 +26,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting a server at: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- story.go
+++ story.go
@@ -3,7 +3,50 @@ package cyoa
 import (
 	"encoding/json"
 	"io"
+	"net/http"
+	"text/template"
 )
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTmpl = `
+	<!DOCTYPE html>
+	<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Choose your own adventure!</title>
+    </head>
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+        <p>{{.}}</p>
+        {{end}}
+        <ul>
+        {{range .Options}}
+            <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+        </ul>
+    </body>
+	</html>`
+
+func NewHandler(s Story) handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 
@@ -24,6 +67,6 @@ type Chapter struct {
 }
 
 type Option struct {
-	Text string `json:"text"`
-	Arc  string `json:"arc"`
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
 }
