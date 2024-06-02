73dc9872d0f973a47d855e8781adf4ef0b1398cc
nemopss <alexey_gladilin@mail.ru>
2024-06-02 19:17:55 +0300

    Added styling to HTML

--- story.go
+++ story.go
@@ -22,17 +22,63 @@ var defaultHandlerTmpl = `
         <meta charset="UTF-8">
         <title>Choose your own adventure!</title>
     </head>
-    <body>
-        <h1>{{.Title}}</h1>
-        {{range .Paragraphs}}
+<body>
+    <section class="page">
+      <h1>{{.Title}}</h1>
+      {{range .Paragraphs}}
         <p>{{.}}</p>
-        {{end}}
+      {{end}}
+      {{if .Options}}
         <ul>
         {{range .Options}}
-            <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+          <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
         {{end}}
         </ul>
-    </body>
+      {{else}}
+        <h3>The End</h3>
+      {{end}}
+    </section>
+    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FFFCF6;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #777;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: none;
+        color: #6295b5;
+      }
+      a:active,
+      a:hover {
+        color: #7792a2;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+  </body>
 	</html>`
 
 func NewHandler(s Story) handler {
