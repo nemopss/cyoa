d5822e1693dba0ed13bca779bdcfeb2ddc0171c6
nemopss <alexey_gladilin@mail.ru>
2024-06-02 16:07:07 +0300

    Added types for JSON parsing

---
+++ story.go
@@ -0,0 +1,14 @@
+package cyoa
+
+type Stopy map[string]Chapter
+
+type Chapter struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
+}
+
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
